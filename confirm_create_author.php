57a1035b95b8793300132f7338db11fcade8eeab
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-03-30 16:25:51 +0100

    Fix de l'ajout d'auteur et var dump enlevé

--- confirm_create_author.php
+++ confirm_create_author.php
@@ -1,11 +1,10 @@
 <?php
 $title = 'Confirmation de création de l\'auteur';
 include './header.php';
-var_dump($_POST);
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['author_name']) ){
     $author_name = $_POST['author_name'];
     $query = "  INSERT INTO author (`name`)
-    VALUES (:nameprotected,);";
+    VALUES (:nameprotected);";
     $statement = $pdo->prepare($query);
     $statement->bindValue(':nameprotected', $author_name, \PDO::PARAM_STR);
     $statement->execute();
