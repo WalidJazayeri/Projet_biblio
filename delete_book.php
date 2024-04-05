6b1104446ca508c725e1e7da6f25c907d587eb98
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-05 13:27:45 +0200

    rajout redirection apres suppression de livre

--- delete_book.php
+++ delete_book.php
@@ -11,6 +11,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id']) )
     $statement = $pdo->prepare($query);
     $statement->bindValue(':idprotege', $idBook, \PDO::PARAM_INT);
     $statement->execute();
+    echo "<p>Livre supprimé (redirection en cours...)</p>";
+    header('refresh:3;url=list_books.php');
 }
 include 'footer.php';
 ?>
