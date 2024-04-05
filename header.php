8fd62de31a60b2e38d6dc0d01cf1e27ba14d9e0d
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-05 19:52:39 +0200

    alain

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ echo '<!doctype html>
 <a href="list_authors.php">Liste des auteurs</a>
 <div>
     <a href="todo.php">Todo List</a>
+    <h1>SALUT ALAIN</h1>
 </div>';
 
 require_once 'connec.php';
